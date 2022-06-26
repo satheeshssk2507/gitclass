b7f4990d066f896361689adfb70dc28f8c0eed02
satheeshssk <s.satheeshkumar.ssk@gmail.com>
2022-06-26 08:37:19 +0530

    firsh push code

---
+++ src/org/gitclsaa/gitsample.java
@@ -0,0 +1,15 @@
+package org.gitclsaa;
+
+public class gitsample {
+
+	private void sample1() {
+	System.out.println("1");
+	}
+	
+	private void sample2() {
+		System.out.println("2");
+
+	}
+	
+	
+}
